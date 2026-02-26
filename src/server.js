bef7a1d017a16cdd7b51d4548285bc93890a889e
gurumukhahuja-ux <gurumukhahuja1@gmail.com>
2026-02-26 17:17:45 +0530

    hy

--- src/server.js
+++ src/server.js
@@ -66,7 +66,7 @@ app.use((err, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => {
     console.log(`✅ Server running on port ${PORT}`);
